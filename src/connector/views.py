3f41d52fbc9d7783f9856383477d14a30a126bdb
Luc Galarneau <lgalarno@gmail.com>
2024-04-02 18:17:19 -0400

    few cleanup

--- src/connector/views.py
+++ src/connector/views.py
@@ -76,12 +76,12 @@ def connect_webid(request, pk):
 def disconnect_webid(request):
     request.session['web_id'] = None
     request.session['session_pk'] = None
-    return redirect('home')
+    return redirect('pod_registration:dashboard')
 
 
 def disconnect_session(request):
     request.session['state_session'] = None
-    return redirect('pod_registration:dashboard')
+    return redirect('home')
 
 
 def connect_oidc(request):
@@ -121,7 +121,6 @@ def connect_oidc(request):
         return redirect('pod_registration:dashboard')
 
 
-#TODO fix oauth_callback to work with pod session
 def connect_oidc_session(request):
     context = {}
     if request.method == 'POST':
@@ -138,7 +137,6 @@ def connect_oidc_session(request):
                 state = make_random_string()
                 session_updt, query = client_registration(state=state, registration_endpoint=registration_endpoint)
                 if session_updt:
-                    #TODO do we need to keep provider_info? need token_endpoint
                     request.session['state_session'] = {
                         'token_endpoint': provider_info['token_endpoint'],
                         'state': state,
@@ -170,11 +168,9 @@ def oauth_callback(request):
     """
     auth_code = request.GET.get('code', None)
     state = request.GET.get('state', None)
-    #TODO make code better
-    state_session_obj = StateSession.objects.filter(state=state).first()
+    state_session_obj = StateSession.objects.filter(state=state).first()  # if exists in db, then use pod registration model
     if state_session_obj:
         print('state_session FALSE')
-        state_session_obj = get_object_or_404(StateSession, state=state)
         state_session = model_to_dict(state_session_obj)
         provider_info = state_session_obj.oicdp.provider_info
         token_endpoint = provider_info['token_endpoint']
@@ -199,7 +195,7 @@ def oauth_callback(request):
                          },
                          headers={
                              'content-type': 'application/x-www-form-urlencoded',
-                             'DPoP': make_token_for(keypair, token_endpoint, 'POST')  # make_DPoP(token_endpoint, 'POST')  #
+                             'DPoP': make_token_for(keypair, token_endpoint, 'POST')
                          },
                          allow_redirects=False)
     print(f'oauth_callback resp: { resp.json() }')
@@ -219,13 +215,11 @@ def oauth_callback(request):
             request.session['state_session'] = state_session
             # request.session['aaa'] = 'aaa'  # dummy for request.session to be saved...
         else:
-            print('registration login')
             web_id = _update_state_session(resp, state_session=state_session_obj, keypair=keypair)
             #TODO move session_pk and web_id to request.session['state_session'] like above
             request.session['session_pk'] = state_session_obj.pk
             request.session['web_id'] = web_id
     else:
-        print('no token found')
         if state_session_obj.access_token == "" or state_session_obj.access_token is None:
             state_session_obj.delete()
         try:
@@ -241,7 +235,6 @@ def oauth_callback(request):
         except:
             # web server response
             messages.error(request, resp.text)
-
     return HttpResponseRedirect(state_session['redirect_view'])
 
 
@@ -255,7 +248,6 @@ def refresh_token(request):
         request.session['session_pk'] = state_session_obj.pk
     else:
         provider_info = state_session_obj.oicdp.provider_info
-
         # Generate a key-pair.
         keypair = jwcrypto.jwk.JWK.generate(kty='EC', crv='P-256')
         # Exchange auth code for access token
@@ -350,24 +342,20 @@ def session_refresh_token(request):
     return redirect(redirect_uri)  # request
 
 
-#TODO state_session now a dict
 def _update_state_session(response, state_session, keypair) -> str:
     try:
         resp = response.json()
         # update state_session with tokens from the exchange
         at = resp.get('access_token')
         web_id = get_web_id(at)
-
         state_session.access_token = at
         state_session.id_token = resp.get('id_token')
         state_session.DPoP_key = keypair.export()
         state_session.webid = web_id
-
         expires_at = datetime.utcnow() + timedelta(seconds=resp.get('expires_in'))
         state_session.expires_at = timezone.make_aware(expires_at, pytz.UTC, True)
         state_session.token_type = resp.get('token_type')
         state_session.refresh_token = resp.get('refresh_token')
-
         state_session.save()
     except:
         web_id = None
